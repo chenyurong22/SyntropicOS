d8c3fdda4e8ad434b7453347c7279140e5cd7114
Charles Galant <cgalant@gmail.com>
2026-07-25 17:05:12 -0400

    feat(pkg): add PlatformIO library manifest, top-level header & CI release publisher

--- src/SyntropicOS.h
+++ src/SyntropicOS.h
@@ -1,18 +1,11 @@
 /**
  * @file SyntropicOS.h
- * @brief Arduino library discovery header.
- *
- * The Arduino build system requires a header directly in src/ to trigger
- * library detection. This file simply forwards to the real umbrella header.
- *
- * In your sketches you can use either:
- *   #include <SyntropicOS.h>
- *   #include <syntropic/syntropic.h>
+ * @brief Top-level PlatformIO & Arduino umbrella include header.
  */
 
-#ifndef SYNTROPIC_OS_ARDUINO_H
-#define SYNTROPIC_OS_ARDUINO_H
+#ifndef SYNTROPICOS_H
+#define SYNTROPICOS_H
 
 #include "syntropic/syntropic.h"
 
-#endif /* SYNTROPIC_OS_ARDUINO_H */
+#endif /* SYNTROPICOS_H */

ca40ef8efa9fd1dac8f6988595e33a543885c540
Charles Galant <cgalant@gmail.com>
2026-07-27 05:59:01 -0400

    fix(proto): remove POSIX sys/types.h header from syn_isotp.h for Keil compatibility

--- src/syntropic/proto/syn_isotp.h
+++ src/syntropic/proto/syn_isotp.h
@@ -68,10 +68,16 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <sys/types.h>
-#if defined(__AVR__) && !defined(_SSIZE_T_DEFINED_)
-typedef int ssize_t;
+
+#if !defined(_SSIZE_T_DEFINED_) && !defined(_SSIZE_T_DECLARED) && !defined(__ssize_t_defined) && \
+    !defined(_SSIZE_T_) && !defined(_SSIZE_T) && !defined(_SSIZE_T_DEFINED)
+typedef intptr_t ssize_t;
 #define _SSIZE_T_DEFINED_
+#define _SSIZE_T_DECLARED
+#define __ssize_t_defined
+#define _SSIZE_T_
+#define _SSIZE_T
+#define _SSIZE_T_DEFINED
 #endif
 
 #ifdef __cplusplus
